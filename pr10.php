e6a49dc4806b440fb275bfa2704e0071cc4e03c3
Mahesh Bargale <maheshbargale349@gmail.com>
2026-05-13 15:41:35 +0530

    Simplify form handling and remove email field

--- pr10.php
+++ pr10.php
@@ -1,26 +1,22 @@
+<html>
+<body>
+
+<form method="post">
+
+Name: <input type="text" name="name">
+
+<input type="submit">
+
+</form>
+
 <?php
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name =$_POST['name'];
-    $email =$_POST['email'];
+
+if($_POST)
+{
+    echo "Name: " . $_POST['name'];
 }
+
 ?>
 
-<!DOCTYPE html> 
-<html> 
-<body> 
- 
-<form method="post"> 
-    Name: <input type="text" name="name"><br><br> 
-    Email: <input type="email" name="email"><br><br> 
-    <input type="submit" value="Submit"> 
-</form> 
- 
-<?php 
-if (isset($name)) { 
-    echo "<h3>Submitted Data:</h3>"; 
-    echo "Name: " . $name . "<br>"; 
-    echo "Email: " . $email; 
-} 
-?>
 </body>
 </html>
